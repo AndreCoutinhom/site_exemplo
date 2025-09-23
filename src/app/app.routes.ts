9d3508ebc500e9a7e679d1abe1b0d0433ea5cd1b
Andre Coutinho <andre.coutinhom@gmail.com>
2025-09-23 11:01:59 -0300

    Novos componentes router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+import { AlterarComponent } from './pages/alterar/alterar.component';
+import { ConsultarComponent } from './pages/consultar/consultar.component';
+import { ExcluirComponent } from './pages/excluir/excluir.component';
+import { ListarComponent } from './pages/listar/listar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'', redirectTo:'cadastrar'}, //se nenhum botão tiver sido apertado, ir direto para o componente de cadastro
+    {path: 'cadastrar', component: CadastrarComponent, title: 'Cadastro'},
+    {path: 'alterar', component: AlterarComponent, title: 'Alteração'},
+    {path: 'consultar', component: ConsultarComponent, title: 'Consulta'},
+    {path: 'excluir', component: ExcluirComponent, title: 'Exclusão'},
+    {path: 'listar', component: ListarComponent, title: 'Listagem'},
+    {path:'**', redirectTo:'listar'} //se o link for quebrado abra o componente de listagem
+
+];
