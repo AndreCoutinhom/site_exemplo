a5aeeab8edf038aa48df94a56b49d088e5521d2a
Andre Coutinho <andre.coutinhom@gmail.com>
2025-09-23 10:08:08 -0300

    Novo Site em Angular com cores do logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FootnoteComponent } from './footnote/footnote.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FootnoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/footnote/footnote.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footnote',
+  imports: [],
+  templateUrl: './footnote.component.html',
+  styleUrl: './footnote.component.css'
+})
+export class FootnoteComponent {
+
+}
